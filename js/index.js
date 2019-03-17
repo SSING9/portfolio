d1426be05fe5e81447529955ba101fb7b8cb2dd9
SSING9 <46590219+SSING9@users.noreply.github.com>
2019-03-17 21:55:44 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+$('.ScrollDownClick').scrollToAnchor({
+  speed : 3000
+});
+
 $(document).ready(function(){
-  $("#headerTile").hide();
-  $("#headerTile").fadeIn(1500);
-  $("#header").hide();
-  $("#header").fadeIn(2000);
+  $('#headerTile').hide();
+  $('#headerTile').fadeIn(2000);
+  $('#ScrollDown').hide();
+  $('#ScrollDown').fadeIn(3000);
 });
