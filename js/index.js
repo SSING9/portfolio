874d93d16f46a62bef394c9504c63bfea0114b22
SSING9 <46590219+SSING9@users.noreply.github.com>
2019-03-20 16:19:01 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,38 @@
-$('.ScrollDownClick').scrollToAnchor({
-  speed : 3000
+$(document).ready(function() {
+  $('#topBtn').hide();
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 1100) {
+      $('#topBtn').fadeIn(500);
+    } else {
+      $('#topBtn').fadeOut(500);
+    };
+  });
 });
 
-$(document).ready(function(){
+
+$(document).ready(function() {
+  $('#topBtnA').click(function(event) {
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 700);
+  });
+});
+
+
+$(document).ready(function() {
+  $('.ScrollDownClick').click(function(event) {
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 700);
+  });
+});
+
+
+
+$(document).ready(function() {
   $('#headerTile').hide();
   $('#headerTile').fadeIn(2000);
   $('#ScrollDown').hide();
